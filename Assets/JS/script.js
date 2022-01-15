b9147d5a3358fabb93390e7352335e07a5086fda
Matt DSena <matthewdsena@gmail.com>
2022-01-15 22:17:38 +1030

    think its working, need to have a look tomorrow, when the time is on

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,2 +1,45 @@
+var saveBtn = $(".saveBtn");
+
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    localStorage.setItem(time, plan);
+});
+
+function usePlanner() {
+
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+timeBlockColor();
+usePlanner();
+
